bc424d283670d52e2b736704aad0f471f8acdabc
XThinct <bryantnathaniel04@gmail.com>
2024-09-10 22:00:01 +0700

    Tugas PBKK D LARAVEL Bryant

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,17 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h3 class="text-xl">
+      Welcome to My Contact Page!
+    </h3>
+    <br>
+    <ul>
+        <li>
+            <a href="https://instagram.com/bryantnath_">- Instagram</a>
+        </li>
+    
+        <li>
+            <a href="https://x.com/xthinct_">- X</a>
+        </li>
+    </ul>
+  </x-layout>
+
