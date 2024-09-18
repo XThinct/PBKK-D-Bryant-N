9315a05bf0215b3d076fd3d1cf07906dd40a0984
XThinct <bryantnathaniel04@gmail.com>
2024-09-18 18:02:06 +0700

    Update Laravel Project

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -23,13 +23,5 @@
       </div>
     </main>
   </div>
-  
-    {{-- <a href="/">Home</a>
-    <a href="/about">About</a>
-    <a href="/blog">Blog</a>
-    <a href="/contact">Contact</a>
-    <h1>Home Page</h1>
-
-<script src="js/script.js"></script> --}}
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,28 @@
 <?php
 
+use App\Models\Post;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
 });
 
-Route::get('/about', function () {  
-    return view('about', ['name' => 'Bryant Nathaniel T'], ['title' => 'About Page'] );
+Route::get('/about', function () {
+    return view('about', ['name' => 'Bryant Nathaniel Tanujaya', 'title' => 'About Page']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog']);
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog Page', 'posts' => Post::all()]);
+});
+
+Route::get('/posts/{slug}', function ($slug) {
+  
+    $post = Post::find($slug);
+
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
 Route::get('/contact', function () {
-    return view('contact', ['title' => 'Contact']);
+    return view('contact', ['title' => 'Contact Page']);
 });
